29de8f244c2f423bc1edee5a3b6aaef41605415c
sreejith <squeeky.mouse@gmail.com>
2023-03-06 09:54:57 +0530

    update db path Sec40

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 # Establish a connection
-dbconnection=sqlite3.connect('db/data.sqlite')
+dbconnection=sqlite3.connect('data.sqlite')
 
 def scrape(url):
     """Scrape the page source from the url"""
